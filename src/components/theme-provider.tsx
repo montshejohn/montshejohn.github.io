d606c193ac24798e4745e87a11d761d640cb2e8e
J Mokgokong <jmokgokong@Js-MacBook-Pro.local>
2025-09-18 01:41:20 +0200

    Update ThemeProviderProps interface with missing properties

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -7,6 +7,8 @@ type ThemeProviderProps = {
   children: React.ReactNode
   attribute?: string
   defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
 }
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
